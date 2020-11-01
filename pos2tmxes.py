88750c59aeb4b16d3eb377ccb0aadf606fca0e4c
Maciej Olko <maciej.olko@yougov.com>
2020-11-01 22:40:19 +0100

    Add converting script

---
+++ pos2tmxes.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from os import walk
+from pathlib import Path
+from subprocess import call
+
+
+def convert():
+    for dirpath, dirnames, filenames in walk('pos/locale/python'):
+        print(dirpath, dirnames, filenames)
+        for filename in filenames:
+            if filename.endswith('.po'):
+                directory = Path(dirpath)
+                language = directory.name
+                source = directory / filename
+                target_directory = Path('tmxes') / language
+                target = target_directory / (source.stem + '.tmx')
+                target_directory.mkdir(parents=True, exist_ok=True)
+                call(
+                    f'po2tmx --language {language} {source} {target}',
+                    shell=True
+                )
+
+
+if __name__ == '__main__':
+    convert()
