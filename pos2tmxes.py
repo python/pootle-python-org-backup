861b25251902895f624b9a6459505c5588ce91e9
Maciej Olko <maciej.olko@yougov.com>
2020-11-01 22:49:51 +0100

    Improve converting script
    Remove print, use f-string.

--- pos2tmxes.py
+++ pos2tmxes.py
@@ -6,14 +6,13 @@ from subprocess import call
 
 def convert():
     for dirpath, dirnames, filenames in walk('pos/locale/python'):
-        print(dirpath, dirnames, filenames)
         for filename in filenames:
             if filename.endswith('.po'):
                 directory = Path(dirpath)
                 language = directory.name
                 source = directory / filename
                 target_directory = Path('tmxes') / language
-                target = target_directory / (source.stem + '.tmx')
+                target = target_directory / f'{source.stem}.tmx'
                 target_directory.mkdir(parents=True, exist_ok=True)
                 call(
                     f'po2tmx --language {language} {source} {target}',
